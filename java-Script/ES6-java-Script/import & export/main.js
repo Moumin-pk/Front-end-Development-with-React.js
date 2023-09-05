9f1e483b9f580bdb443a2996cba7bb8a7d25425f
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2023-09-05 11:30:11 +0500

    Learning Journey in Java-Script

---
+++ java-Script/ES6-java-Script/import & export/main.js
@@ -0,0 +1,7 @@
+import {add,sub} from "./utility";
+const a =5;
+const b =10;
+const y =5;
+const x =10;
+console.log(add(a,b));
+console.log(sub(x,y));
---
+++ java-Script/ES6-java-Script/import & export/utility.js
@@ -0,0 +1,7 @@
+export const add = (a,b) => {
+    return a+b;
+};
+
+export const sub = (x,y) =>{
+    return x-y;
+};
